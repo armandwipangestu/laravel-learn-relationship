ad7a73eecb57ade53f843927ac69894dcf9570d9
Arman <armandwi.pangestu7@gmail.com>
2024-01-17 09:49:37 +0700

    feat: one to many invers relationship A.K.A belongsTo

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,10 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    // Now that we can access all of a post's comments, let's define a relationship to
+    // allow a comment to access its parent post. To define the inverse of a hasMany
+    // relationship, define a relationship method on the child model which calls the
+    // belongsTo method:
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
